c4344b16c14480536512d76900390ad323474870
Fabian Sturm <f@rtfs.org>
2016-06-10 09:10:34 +0200

    Rename project.

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'package:angular2/platform/browser.dart';
-import 'package:angular2_getting_started/app_component.dart';
+import 'package:angular2_dart/app_component.dart';
 
 void main() {
   bootstrap(AppComponent);
