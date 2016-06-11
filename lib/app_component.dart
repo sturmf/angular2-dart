4a78c57cfdeee79a2ac212c23eb081e3e00c5ac8
Fabian Sturm <f@rtfs.org>
2016-06-11 14:07:55 +0200

    Finished heroes tutorial pt4.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,91 +1,84 @@
 import 'package:angular2/core.dart';
+import 'hero.dart';
+import 'hero_detail_component.dart';
 
-class Hero {
-  final int id;
-  String name;
-
-  Hero(this.id, this.name);
-}
 
 @Component(
-    selector: 'my-app',
-    styles: const [
-      '''
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 10em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0em;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #EEE;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0em 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0px 0px 4px;
-      }
+  selector: 'my-app',
+  styles: const [
     '''
-    ],
-    template: '''
-      <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 10em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0em;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #EEE;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0em 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0px 0px 4px;
+    }
+  '''
+  ],
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
 
-      <ul class="heroes">
-        <li *ngFor="#hero of heroes"
-          [class.selected]="hero == selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero == selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
 
-      <div *ngIf="selectedHero != null">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <div><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
-        </div>
-      </div>''' )
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    ''',
+    directives: const [
+      HeroDetailComponent
+    ])
 class AppComponent {
   final List<Hero> heroes = mockHeroes;
   String title = 'Tour of Heroes';
   Hero selectedHero;
 
   onSelect(Hero hero) {
-      selectedHero = hero;
+    selectedHero = hero;
   }
 }
 
